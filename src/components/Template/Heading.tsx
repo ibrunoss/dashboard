a617b285d99fc0bd77331357d043f49536303c51
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-07 20:09:02 -0300

    style: import poppins font to Heading

--- src/components/Template/Heading.tsx
+++ src/components/Template/Heading.tsx
@@ -1,5 +1,6 @@
-import Title from "./Title";
+import Head from "next/head";
 
+import Title from "./Title";
 interface HeadingProps {
   title: string;
   caption: string;
@@ -8,9 +9,21 @@ interface HeadingProps {
 const Heading: React.FC<HeadingProps> = (props) => {
   const { title, caption } = props;
   return (
-    <div>
-      <Title title={title} caption={caption} />
-    </div>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <Title title={title} caption={caption} />
+      </div>
+    </>
   );
 };
 
