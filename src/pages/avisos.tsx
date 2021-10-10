310eb1ad4bf59213c3bf3946fb000b80d35b2e37
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-10 16:51:50 -0300

    feat: add avisos page

---
+++ src/pages/avisos.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Template/Layout";
+
+export default function Avisos() {
+  return (
+    <Layout title="Avisos" caption="Aqui você visualizará seus avisos">
+      Conteudo
+    </Layout>
+  );
+}
