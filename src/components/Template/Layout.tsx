77195b2a91a178d2d484b4281316c51863e92ac0
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-09 23:47:45 -0300

    component: Layout updated
    Added struct for dark theme

--- src/components/Template/Layout.tsx
+++ src/components/Template/Layout.tsx
@@ -5,14 +5,20 @@ import SideMenu from "./SideMenu";
 interface LayoutProps {
   title: string;
   caption: string;
+  dark?: boolean;
 }
 
 const Layout: React.FC<LayoutProps> = (props) => {
-  const { title, caption, children } = props;
+  const { title, caption, dark, children } = props;
+
+  const className: string[] = ["flex", "min-h-screen", "min-w-screen"];
+
+  dark && className.push("dark");
+
   return (
-    <div className={`flex min-h-screen min-w-screen`}>
+    <div className={className.join(" ")}>
       <SideMenu />
-      <div className="flex flex-col w-full p-7 bg-gray-300">
+      <div className="flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800">
         <Heading title={title} caption={caption} />
         <Content>{children}</Content>
       </div>
