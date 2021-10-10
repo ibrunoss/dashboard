bd7368b2943ad2556334b89467423cb267ca26c0
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-10 16:50:55 -0300

    refactor: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from "next/head";
+import Layout from "../components/Template/Layout";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Admin Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1>Admin</h1>
-    </div>
+    <Layout title="Admin Page" caption="Administrators private page">
+      Conteudo
+    </Layout>
   );
 }
