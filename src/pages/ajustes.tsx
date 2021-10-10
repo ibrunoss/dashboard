a6c4ca2160d6485f612b1c73c9a31c180f9bead7
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-10 16:51:36 -0300

    feat: add ajustes page

---
+++ src/pages/ajustes.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Template/Layout";
+
+export default function Ajustes() {
+  return (
+    <Layout
+      title="Ajustes & Configurações"
+      caption="Página para personalizar o sistema"
+    >
+      Conteudo
+    </Layout>
+  );
+}
