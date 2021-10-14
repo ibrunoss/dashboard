196555d55d63dd17009f7fc97a7d70999390c21d
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-13 21:29:54 -0300

    feat: updated MenuItem component

--- src/components/Template/MenuItem.tsx
+++ src/components/Template/MenuItem.tsx
@@ -1,22 +1,32 @@
 import Link from "next/link";
 
 interface MenuItemProps {
-  url: string;
   label: string;
   icon: React.ReactNode;
+  url?: string;
+  onClick?: (ev: React.MouseEvent) => void;
+  className?: string;
 }
 
 const MenuItem: React.FC<MenuItemProps> = (props) => {
-  const { url, label, icon } = props;
+  const { url, label, icon, onClick, className } = props;
   console.log();
+  const classNameList: string[] = ["hover:bg-gray-100", "cursor-pointer"];
+
+  if (className) {
+    classNameList.push(className);
+  }
+
+  const renderItem = () => (
+    <a className="flex flex-col justify-center items-center h-20 w-20">
+      {icon}
+      <span className="text-xs font-light text-gray-600">{label}</span>
+    </a>
+  );
+
   return (
-    <li className="hover:bg-gray-100 cursor-pointer">
-      <Link href={url}>
-        <a className="flex flex-col justify-center items-center h-20 w-20">
-          {icon}
-          <span className="text-xs font-light text-gray-600">{label}</span>
-        </a>
-      </Link>
+    <li onClick={onClick} className={classNameList.join(" ")}>
+      {url ? <Link href={url}>{renderItem()}</Link> : renderItem()}
     </li>
   );
 };
