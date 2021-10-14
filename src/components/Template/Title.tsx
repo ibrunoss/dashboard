50ca6381cb1c24b815dfa5ea97f391cd9c4c4cb1
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-13 21:33:58 -0300

    feat: updated Title component

--- src/components/Template/Title.tsx
+++ src/components/Template/Title.tsx
@@ -7,8 +7,8 @@ const Title: React.FC<TitleProps> = (props) => {
   const { title, caption } = props;
   return (
     <div>
-      <h1>{title}</h1>
-      <h2>{caption}</h2>
+      <h1 className="font-black text-3xl text-gray-900">{title}</h1>
+      <h2 className="text-light text-sm text-gray-600">{caption}</h2>
     </div>
   );
 };
