a2bc29c13456e61c37af2829c47e526dc4fb2b85
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-25 00:34:17 -0300

    component: updated Input
    - Add style

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -11,9 +11,10 @@ interface InputProps {
 const Input: React.FC<InputProps> = (props) => {
   const { label, onChange, value, type, required } = props;
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mt-4">
       <label>{label}</label>
       <input
+        className="px-4 py-3 rounded-lg bg-gray-200 mt-1 border focus:border-blue-500 focus:outline-none focus:bg-gray-50"
         type={type || "text"}
         onChange={({ target }) => onChange?.(target.value)}
         value={value}
