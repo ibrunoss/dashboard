9acf47d00c9f214e633a7666f46c2e08a17b8f89
Bruno Silva Santana <ibrunoss@outlook.com>
2022-10-29 00:16:01 -0300

    component: update login page
    - Add random image
    - Add link to sign in/sign up

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,71 +4,98 @@ import { useState } from "react";
 import Input from "../components/form/Input";
 
 export default function Login() {
-  const [mode, setMode] = useState<"signIn" | "signUp">("signIn");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [mode, setMode] = useState<"signIn" | "signUp">("signIn");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  interface Actions {
-    signIn: () => void;
-    signUp: () => void;
-  }
+    interface Actions {
+        signIn: () => void;
+        signUp: () => void;
+    }
 
-  const actions: Actions = {
-    signIn() {
-      console.log("entrar");
-    },
-    signUp() {
-      console.log("cadastrar");
-    },
-  };
+    const actions: Actions = {
+        signIn() {
+            console.log("entrar");
+        },
+        signUp() {
+            console.log("cadastrar");
+        },
+    };
 
-  function submit() {
-    actions[mode]();
-  }
+    const setSignIn = () => setMode("signIn");
+    const setSignUp = () => setMode("signUp");
 
-  return (
-    <>
-      <Head>
-        <title>Login - {mode === "signIn" ? "Entrar" : "Cadastrar"}</title>
-      </Head>
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <div className="w-1/2 max-w-md">
-          <h1 className="text-xl font-bold mb-5">
-            {mode === "signIn"
-              ? "Entre com sua conta"
-              : "Cadastre-se na plataforma"}
-          </h1>
-          <Input
-            type="email"
-            label="Email"
-            value={email}
-            onChange={setEmail}
-            required
-          />
-          <Input
-            type="password"
-            label="Password"
-            value={password}
-            onChange={setPassword}
-            required
-          />
-          <button
-            className="w-full bg-indigo-500 hover:bg-indigo-400 rounded-lg text-white px-4 py-3 mt-6"
-            onClick={submit}
-          >
-            {mode === "signIn" ? "Entrar" : "Cadastrar"}
-          </button>
+    function submit() {
+        actions[mode]();
+    }
 
-          <hr className="my-6 border-gray-300 w-full" />
+    return (
+        <>
+            <Head>
+                <title>
+                    Login - {mode === "signIn" ? "Entrar" : "Cadastrar"}
+                </title>
+            </Head>
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="hidden md:block md:w-1/2 lg:w-2/3">
+                    <img
+                        className="h-screen w-full object-cover"
+                        src="https://source.unsplash.com/random"
+                        alt="Imagem Aleatória"
+                    />
+                </div>
+                <div className="m-10  w-full md:w-1/2 lg:w-1/3">
+                    <h1 className="text-xl md:text-2xl lg:text-2xl font-bold mb-5">
+                        {mode === "signIn"
+                            ? "Entre com sua conta"
+                            : "Cadastre-se na plataforma"}
+                    </h1>
+                    <Input
+                        type="email"
+                        label="Email"
+                        value={email}
+                        onChange={setEmail}
+                        required
+                    />
+                    <Input
+                        type="password"
+                        label="Password"
+                        value={password}
+                        onChange={setPassword}
+                        required
+                    />
+                    <button
+                        className="w-full bg-indigo-500 hover:bg-indigo-400 rounded-lg text-white px-4 py-3 mt-6"
+                        onClick={submit}
+                    >
+                        {mode === "signIn" ? "Entrar" : "Cadastrar"}
+                    </button>
 
-          <button
-            className="w-full bg-red-500 hover:bg-red-400 rounded-lg text-white px-4 py-3"
-            onClick={submit}
-          >
-            {mode === "signIn" ? "Entrar" : "Cadastrar"} com Google
-          </button>
-        </div>
-      </div>
-    </>
-  );
+                    <hr className="my-6 border-gray-300 w-full" />
+
+                    <button
+                        className="w-full bg-red-500 hover:bg-red-400 rounded-lg text-white px-4 py-3"
+                        onClick={submit}
+                    >
+                        {mode === "signIn" ? "Entrar" : "Cadastrar"} com Google
+                    </button>
+                    <p className="mt-8 text-center">
+                        <a
+                            onClick={mode === "signIn" ? setSignUp : setSignIn}
+                            className="cursor-pointer"
+                        >
+                            {mode === "signIn"
+                                ? "Novo por aqui?"
+                                : "Já faz parte da nossa comunidade?"}{" "}
+                            <span className="text-blue-500 hover:text-blue-700 font-semibold">
+                                {mode === "signIn"
+                                    ? "Crie uma conta gratuitamente"
+                                    : "Entre com as suas credenciais"}
+                            </span>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
 }
